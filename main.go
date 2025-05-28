e8fac5ab951f364334a9234c7483cbcb0cc23cac
Pawitross <pabialek@us.edu.pl>
2025-05-28 12:06:40 +0200

    Changed the response format to non-indented JSON, grouped Gin routes and added POST method for adding new book

--- main.go
+++ main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"strconv"
 	"net/http"
+	"strconv"
 
-	"pawrest/sqldb"
 	"github.com/gin-gonic/gin"
+	"pawrest/sqldb"
 )
 
 func getBooks(c *gin.Context) {
 	books, err := sqldb.GetKsiazki()
-
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
+		c.JSON(http.StatusNotFound, gin.H{"error": "Nie znaleziono książek"})
+		return
 	}
 
-	c.IndentedJSON(http.StatusOK, books)
+	c.JSON(http.StatusOK, books)
 }
 
 func getBook(c *gin.Context) {
@@ -25,13 +25,33 @@ func getBook(c *gin.Context) {
 	id, _ := strconv.Atoi(idStr)
 
 	book, err := sqldb.GetKsiazka(int64(id))
-
 	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Nie znaleziono książki"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Nie znaleziono książki"})
 		return
 	}
 
-	c.IndentedJSON(http.StatusOK, book)
+	c.JSON(http.StatusOK, book)
+}
+
+func postBook(c *gin.Context) {
+	var newBook sqldb.Ksiazka
+
+	if err := c.BindJSON(&newBook); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Wystąpił problem z JSON"})
+		fmt.Println("BŁĄD bind:", err)
+		return
+	}
+
+	id, err := sqldb.InsertKsiazka(newBook)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Nie udało się dodać książki - sprawdź składnię danych"})
+		fmt.Println("BŁĄD insert:", err)
+		return
+	}
+
+	newBook.Id = id
+
+	c.JSON(http.StatusCreated, newBook)
 }
 
 func deleteBook(c *gin.Context) {
@@ -39,24 +59,34 @@ func deleteBook(c *gin.Context) {
 	id, _ := strconv.Atoi(idStr)
 
 	if err := sqldb.DelKsiazka(int64(id)); err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Nie znaleziono książki"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Nie znaleziono książki"})
 		return
 	}
 
-	c.Status(204)
+	c.Status(http.StatusNoContent)
 }
 
 func main() {
-	fmt.Println("Starting the server...")
+	fmt.Println("Łączenie z bazą danych...")
 	sqldb.ConnectToDB()
+	defer sqldb.Db.Close()
 
+	fmt.Println("Uruchamianie serwera...")
 	//gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
-	router.GET("api/v1/books", getBooks)
-	router.GET("api/v1/books/:id", getBook)
+	api := router.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		{
+			v1.GET("books", getBooks)
+			v1.GET("books/:id", getBook)
 
-	router.DELETE("api/v1/books/:id", deleteBook)
+			v1.POST("books", postBook)
 
-	router.Run("localhost:8080")
+			v1.DELETE("books/:id", deleteBook)
+		}
+	}
+
+	router.Run(":8080")
 }

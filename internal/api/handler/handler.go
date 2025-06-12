7c1e872c0d64206ad725fe46f25ee67884460025
Pawitross <pabialek@us.edu.pl>
2025-06-12 11:54:07 +0200

    Add documentation to book handlers and generate docs

--- internal/api/handler/handler.go
+++ internal/api/handler/handler.go
@@ -9,12 +9,22 @@ import (
 	m "pawrest/internal/models"
 )
 
-//	@Summary		Get list of books in an array
-//	@Description	Responds with the list of all books as JSON.
-//	@Tags			Books
-//	@Produce		json
-//	@Success		200	{array}	models.Book
-//	@Router			/books [get]
+// @Summary		Get a list of all books
+// @Description	Responds with a list of all books as JSON. Optional filtering and pagination is available through parameters.
+// @Tags			Books
+// @Produce		json
+// @Param			id			query		string				false	"Book id"
+// @Param			title		query		string				false	"Book title"
+// @Param			year		query		int					false	"Year of publishing of the book"
+// @Param			pages		query		int					false	"Number of pages in the book"
+// @Param			author		query		int					false	"Author id"
+// @Param			genre		query		int					false	"Genre id"
+// @Param			language	query		int					false	"Language id"
+// @Param			limit		query		int					false	"Limit returned number of resources"
+// @Param			offset		query		int					false	"Offset returned resources"
+// @Success		200			{array}		models.Book			"OK - Fetched books"
+// @Failure		404			{object}	map[string]string	"Not found"
+// @Router			/books [get]
 func GetBooks(c *gin.Context) {
 	params := c.Request.URL.Query()
 
@@ -27,6 +37,15 @@ func GetBooks(c *gin.Context) {
 	c.JSON(http.StatusOK, books)
 }
 
+// @Summary		Get one book
+// @Description	Responds with the queried book as JSON or an error message.
+// @Tags			Books
+// @Produce		json
+// @Param			id	path		int					true	"Book id"
+// @Success		200	{object}	models.Book			"OK - Fetched book"
+// @Failure		400	{object}	map[string]string	"Bad Request - Invalid book id"
+// @Failure		404	{object}	map[string]string	"Not found"
+// @Router			/books/{id} [get]
 func GetBook(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
@@ -44,6 +63,16 @@ func GetBook(c *gin.Context) {
 	c.JSON(http.StatusOK, book)
 }
 
+// @Summary		Create a new book
+// @Description	Accepts a JSON body to create a new book. Responds with the created book and set Location header or an error message.
+// @Tags			Books
+// @Accept			json
+// @Produce		json
+// @Param			book	body		models.Book			true	"New Book"
+// @Success		201		{object}	models.Book			"Created - Added new book"
+// @Failure		400		{object}	map[string]string	"Bad Request - Invalid input or JSON"
+// @Header			201		{string}	Location			"Path of the newly created book"
+// @Router			/books [post]
 func PostBook(c *gin.Context) {
 	var newBook m.Book
 
@@ -71,6 +100,16 @@ func PostBook(c *gin.Context) {
 	c.JSON(http.StatusCreated, newBook)
 }
 
+// @Summary		Update an existing book
+// @Description	Accepts a JSON body to update a book. Responds with a status code. When an error occurs the response body contains JSON data with the message.
+// @Tags			Books
+// @Accept			json
+// @Param			id		path	int			true	"Existing Book id"
+// @Param			book	body	models.Book	true	"Updated Book"
+// @Success		204		"Updated the book"
+// @Failure		400		{object}	map[string]string	"Bad Request - Invalid input or JSON"
+// @Failure		409		{object}	map[string]string	"Conflict -  No resource found or didn't change resource"
+// @Router			/books/{id} [put]
 func PutBook(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
@@ -99,6 +138,16 @@ func PutBook(c *gin.Context) {
 	c.Status(http.StatusNoContent)
 }
 
+// @Summary		Patch an existing book
+// @Description	Accepts a JSON body with patch data to a book. Responds with a status code. When an error occurs the response body contains JSON data with the message.
+// @Tags			Books
+// @Accept			json
+// @Param			id		path	int			true	"Existing Book id"
+// @Param			book	body	models.Book	true	"Patches to the book"
+// @Success		204		"No Content - Successfully patched the book"
+// @Failure		400		{object}	map[string]string	"Bad Request - Invalid input or JSON"
+// @Failure		404		{object}	map[string]string	"Not found -  No resource found or didn't change resource"
+// @Router			/books/{id} [patch]
 func PatchBook(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
@@ -122,6 +171,13 @@ func PatchBook(c *gin.Context) {
 	c.Status(http.StatusNoContent)
 }
 
+// @Summary		Delete an existing book
+// @Description	Responds with a status code. When an error occurs the response body contains an error message.
+// @Tags			Books
+// @Param			id	path	int	true	"Book id"
+// @Success		204	"No Content - Successfully deleted the book"
+// @Failure		400	{object}	map[string]string	"Bad Request - Invalid book id"
+// @Router			/books/{id} [delete]
 func DeleteBook(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)

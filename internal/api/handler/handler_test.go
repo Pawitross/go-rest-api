233fb344310e5a4aa00141d1ca619a49d66b7d4a
Pawitross <pabialek@us.edu.pl>
2025-06-21 15:37:36 +0200

    Fix runMain func error return, extract repeating code to separate funcs and add integration tests

--- internal/api/handler/handler_test.go
+++ internal/api/handler/handler_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -19,6 +20,17 @@ import (
 
 var database *db.Database
 
+func runMain(m *testing.M) (int, error) {
+	var err error
+	database, err = db.ConnectToDB()
+	if err != nil {
+		return 0, err
+	}
+	defer database.CloseDB()
+
+	return m.Run(), nil
+}
+
 func TestMain(m *testing.M) {
 	code, err := runMain(m)
 	if err != nil {
@@ -28,18 +40,7 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-func runMain(m *testing.M) (int, error) {
-	var err error
-	database, err = db.ConnectToDB()
-	if err != nil {
-		return 0, nil
-	}
-	defer database.CloseDB()
-
-	return m.Run(), nil
-}
-
-func SetupTestRouter(db *db.Database) *gin.Engine {
+func setupTestRouter(db *db.Database) *gin.Engine {
 	gin.SetMode(gin.TestMode)
 	router := gin.New()
 
@@ -48,90 +49,93 @@ func SetupTestRouter(db *db.Database) *gin.Engine {
 	return router
 }
 
-// GET /books
-func TestListBooksSuccess(t *testing.T) {
-	router := SetupTestRouter(database)
+func execRequest(method, target string, body io.Reader) *httptest.ResponseRecorder {
+	router := setupTestRouter(database)
 	w := httptest.NewRecorder()
-
-	req := httptest.NewRequest("GET", "/api/v1/books", nil)
+	req := httptest.NewRequest(method, target, body)
 
 	router.ServeHTTP(w, req)
 
+	return w
+}
+
+func marshalCheckNoError(t *testing.T, obj any) []byte {
+	j, err := json.Marshal(obj)
+	assert.NoError(t, err, "JSON marshalling error")
+
+	return j
+}
+
+func decodeBodyCheckEmpty(w *httptest.ResponseRecorder, t *testing.T, obj any) {
+	err := json.NewDecoder(w.Body).Decode(obj)
+	assert.NoError(t, err, "Error decoding response data:", err)
+	assert.NotEmpty(t, obj, "Obj should not be empty")
+}
+
+func checkErrorBodyNotEmpty(w *httptest.ResponseRecorder, t *testing.T) {
+	var rError models.Error
+	decodeBodyCheckEmpty(w, t, &rError)
+}
+
+// GET /books
+func TestListBooksSuccess(t *testing.T) {
+	w := execRequest("GET", "/api/v1/books", nil)
 	assert.Equal(t, http.StatusOK, w.Code)
+
 	var rBooks []models.Book
+	decodeBodyCheckEmpty(w, t, &rBooks)
+}
 
-	err := json.NewDecoder(w.Body).Decode(&rBooks)
-	assert.NoError(t, err, "Error decoding response data")
+func TestListBooksExtSuccess(t *testing.T) {
+	w := execRequest("GET", "/api/v1/books?extend=true", nil)
+	assert.Equal(t, http.StatusOK, w.Code)
 
-	assert.NotEmpty(t, rBooks, "Book slice should not be empty")
+	var rBooks []models.BookExt
+	decodeBodyCheckEmpty(w, t, &rBooks)
 }
 
 func TestListBooksBadRequestUnknownParam(t *testing.T) {
-	router := SetupTestRouter(database)
-	w := httptest.NewRecorder()
-
-	req := httptest.NewRequest("GET", "/api/v1/books?foo=bar", nil)
-
-	router.ServeHTTP(w, req)
-
+	w := execRequest("GET", "/api/v1/books?foo=bar", nil)
 	assert.Equal(t, http.StatusBadRequest, w.Code)
-	var rError models.Error
 
-	err := json.NewDecoder(w.Body).Decode(&rError)
-	assert.NoError(t, err, "Error decoding response data")
+	checkErrorBodyNotEmpty(w, t)
 }
 
 // GET /books/id
 func TestGetBookSuccess(t *testing.T) {
-	router := SetupTestRouter(database)
-	w := httptest.NewRecorder()
-
-	req := httptest.NewRequest("GET", "/api/v1/books/1", nil)
-
-	router.ServeHTTP(w, req)
-
+	w := execRequest("GET", "/api/v1/books/1", nil)
 	assert.Equal(t, http.StatusOK, w.Code)
 
 	var rBook models.Book
-	err := json.NewDecoder(w.Body).Decode(&rBook)
-	assert.NoError(t, err, "Error decoding response data")
+	decodeBodyCheckEmpty(w, t, &rBook)
 
 	assert.NotEmpty(t, rBook, "Book in the response body should not be empty")
 	assert.NotEmpty(t, rBook.Title, "Title should not be empty")
+	assert.NotEmpty(t, rBook.Year, "Year should not be empty")
+	assert.NotEmpty(t, rBook.Pages, "Pages should not be empty")
 	assert.NotEmpty(t, rBook.Author, "Author should not be empty")
 	assert.NotEmpty(t, rBook.Genre, "Genre should not be empty")
 	assert.NotEmpty(t, rBook.Language, "Language should not be empty")
 }
 
-func TestGetBookNotFound(t *testing.T) {
-	router := SetupTestRouter(database)
-	w := httptest.NewRecorder()
-
-	req := httptest.NewRequest("GET", "/api/v1/books/9999", nil)
-
-	router.ServeHTTP(w, req)
-
+func TestGetBookNotFoundBigPathId(t *testing.T) {
+	w := execRequest("GET", "/api/v1/books/9999", nil)
 	assert.Equal(t, http.StatusNotFound, w.Code)
+
+	checkErrorBodyNotEmpty(w, t)
 }
 
-func TestGetBookBadRequest(t *testing.T) {
-	router := SetupTestRouter(database)
-	w := httptest.NewRecorder()
-
-	req := httptest.NewRequest("GET", "/api/v1/books/string", nil)
-
-	router.ServeHTTP(w, req)
-
+func TestGetBookBadRequestStringPathId(t *testing.T) {
+	w := execRequest("GET", "/api/v1/books/string", nil)
 	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+	checkErrorBodyNotEmpty(w, t)
 }
 
 // POST /books
 func TestPostBookSuccess(t *testing.T) {
-	router := SetupTestRouter(database)
-	w := httptest.NewRecorder()
-
 	testBook := models.Book{
-		Title:    "Post test book",
+		Title:    "Post book test",
 		Year:     1996,
 		Pages:    200,
 		Author:   1,
@@ -139,22 +143,16 @@ func TestPostBookSuccess(t *testing.T) {
 		Language: 1,
 	}
 
-	jsonBook, err := json.Marshal(testBook)
-	assert.NoError(t, err, "Book JSON marshalling error")
+	jsonBook := marshalCheckNoError(t, testBook)
 
-	req := httptest.NewRequest("POST", "/api/v1/books", bytes.NewReader(jsonBook))
-
-	router.ServeHTTP(w, req)
-
+	w := execRequest("POST", "/api/v1/books", bytes.NewReader(jsonBook))
 	assert.Equal(t, http.StatusCreated, w.Code)
 
 	var rBook models.Book
-	err = json.NewDecoder(w.Body).Decode(&rBook)
-	assert.NoError(t, err, "Error decoding response data")
+	decodeBodyCheckEmpty(w, t, &rBook)
 	defer database.DelBook(rBook.Id)
 
 	expLoc := fmt.Sprintf("/api/v1/books/%v", rBook.Id)
-
 	assert.Equal(t, expLoc, w.Result().Header.Get("Location"))
 
 	assert.NotZero(t, rBook.Id, "Auto generatated, non zero ID")
@@ -166,10 +164,15 @@ func TestPostBookSuccess(t *testing.T) {
 	assert.Equal(t, testBook.Language, rBook.Language)
 }
 
-func TestPostBookValidationErr(t *testing.T) {
-	router := SetupTestRouter(database)
-	w := httptest.NewRecorder()
+func TestPostBookBadRequestMalformedJSON(t *testing.T) {
+	jsonStr := []byte(`{"title":"JSON Test","year":1996,"pages":200,"author":"Should be number","genre":1}`)
+	w := execRequest("POST", "/api/v1/books", bytes.NewReader(jsonStr))
+	assert.Equal(t, http.StatusBadRequest, w.Code)
 
+	checkErrorBodyNotEmpty(w, t)
+}
+
+func TestPostBookBadRequestValidationErr(t *testing.T) {
 	testBook := models.Book{
 		Title:    "",
 		Year:     1996,
@@ -179,29 +182,36 @@ func TestPostBookValidationErr(t *testing.T) {
 		Language: 1,
 	}
 
-	jsonBook, err := json.Marshal(testBook)
-	assert.NoError(t, err, "Book JSON marshalling error")
+	jsonBook := marshalCheckNoError(t, testBook)
 
-	req := httptest.NewRequest("POST", "/api/v1/books", bytes.NewReader(jsonBook))
-
-	router.ServeHTTP(w, req)
-
+	w := execRequest("POST", "/api/v1/books", bytes.NewReader(jsonBook))
 	assert.Equal(t, http.StatusBadRequest, w.Code)
 
-	var rError models.Error
-	err = json.NewDecoder(w.Body).Decode(&rError)
-	assert.NoError(t, err, "Error decoding response data")
+	checkErrorBodyNotEmpty(w, t)
+}
 
-	assert.NotEmpty(t, rError.Error, "Error message should not be empty")
+func TestPostBookBadRequestForeignKeyErr(t *testing.T) {
+	testBook := models.Book{
+		Title:    "Post foreign key test",
+		Year:     1996,
+		Pages:    200,
+		Author:   1,
+		Genre:    1,
+		Language: 999,
+	}
+
+	jsonBook := marshalCheckNoError(t, testBook)
+
+	w := execRequest("POST", "/api/v1/books", bytes.NewReader(jsonBook))
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+	checkErrorBodyNotEmpty(w, t)
 }
 
 // PUT /books
 func TestPutBookSuccess(t *testing.T) {
-	router := SetupTestRouter(database)
-	w := httptest.NewRecorder()
-
 	testBook := models.Book{
-		Title:    "Książka PUT",
+		Title:    "Put book test",
 		Year:     1996,
 		Pages:    593,
 		Author:   1,
@@ -209,22 +219,15 @@ func TestPutBookSuccess(t *testing.T) {
 		Language: 1,
 	}
 
-	jsonBook, err := json.Marshal(testBook)
-	assert.NoError(t, err, "Book JSON marshalling error")
+	jsonBook := marshalCheckNoError(t, testBook)
 
-	req := httptest.NewRequest("PUT", "/api/v1/books/1", bytes.NewReader(jsonBook))
-
-	router.ServeHTTP(w, req)
-
+	w := execRequest("PUT", "/api/v1/books/1", bytes.NewReader(jsonBook))
 	assert.Equal(t, http.StatusNoContent, w.Code)
 }
 
-func TestPutBookNotFoundBigId(t *testing.T) {
-	router := SetupTestRouter(database)
-	w := httptest.NewRecorder()
-
+func TestPutBookNotFoundBigPathId(t *testing.T) {
 	testBook := models.Book{
-		Title:    "Książka PUT",
+		Title:    "Put book test",
 		Year:     1996,
 		Pages:    593,
 		Author:   1,
@@ -232,28 +235,35 @@ func TestPutBookNotFoundBigId(t *testing.T) {
 		Language: 1,
 	}
 
-	jsonBook, err := json.Marshal(testBook)
-	assert.NoError(t, err, "Book JSON marshalling error")
+	jsonBook := marshalCheckNoError(t, testBook)
 
-	req := httptest.NewRequest("PUT", "/api/v1/books/9999", bytes.NewReader(jsonBook))
-
-	router.ServeHTTP(w, req)
-
+	w := execRequest("PUT", "/api/v1/books/9999", bytes.NewReader(jsonBook))
 	assert.Equal(t, http.StatusNotFound, w.Code)
 
-	var rError models.Error
-	err = json.NewDecoder(w.Body).Decode(&rError)
-	assert.NoError(t, err, "Error decoding response data")
+	checkErrorBodyNotEmpty(w, t)
+}
 
-	assert.NotEmpty(t, rError.Error, "Error message should not be empty")
+func TestPutBookBadRequestForeignKeyErr(t *testing.T) {
+	testBook := models.Book{
+		Title:    "Put foreign key test",
+		Year:     1996,
+		Pages:    593,
+		Author:   1,
+		Genre:    1,
+		Language: 999,
+	}
+
+	jsonBook := marshalCheckNoError(t, testBook)
+
+	w := execRequest("PUT", "/api/v1/books/1", bytes.NewReader(jsonBook))
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+	checkErrorBodyNotEmpty(w, t)
 }
 
 func TestPutBookBadRequestStringId(t *testing.T) {
-	router := SetupTestRouter(database)
-	w := httptest.NewRecorder()
-
 	testBook := models.Book{
-		Title:    "Książka PUT",
+		Title:    "Put book test",
 		Year:     1996,
 		Pages:    593,
 		Author:   1,
@@ -261,45 +271,64 @@ func TestPutBookBadRequestStringId(t *testing.T) {
 		Language: 1,
 	}
 
-	jsonBook, err := json.Marshal(testBook)
-	assert.NoError(t, err, "Book JSON marshalling error")
+	jsonBook := marshalCheckNoError(t, testBook)
 
-	req := httptest.NewRequest("PUT", "/api/v1/books/string", bytes.NewReader(jsonBook))
-
-	router.ServeHTTP(w, req)
-
+	w := execRequest("PUT", "/api/v1/books/string", bytes.NewReader(jsonBook))
 	assert.Equal(t, http.StatusBadRequest, w.Code)
 
-	var rError models.Error
-	err = json.NewDecoder(w.Body).Decode(&rError)
-	assert.NoError(t, err, "Error decoding response data")
+	checkErrorBodyNotEmpty(w, t)
+}
 
-	assert.NotEmpty(t, rError.Error, "Error message should not be empty")
+func TestPutBookBadRequestMalformedJSON(t *testing.T) {
+	jsonStr := []byte(`{"title":"JSON Test","year":1996,"pages":200,"author":"Should be number","genre":1}`)
+	w := execRequest("PUT", "/api/v1/books/1", bytes.NewReader(jsonStr))
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+	checkErrorBodyNotEmpty(w, t)
 }
 
 func TestPutBookBadRequestBadJSON(t *testing.T) {
-	router := SetupTestRouter(database)
-	w := httptest.NewRecorder()
-
 	testBook := models.Book{
-		Title: "Książka PUT",
+		Title: "Put book test",
 		Year:  1996,
 		Pages: 593,
-		Genre: 1,
 	}
 
-	jsonBook, err := json.Marshal(testBook)
-	assert.NoError(t, err, "Book JSON marshalling error")
+	jsonBook := marshalCheckNoError(t, testBook)
 
-	req := httptest.NewRequest("PUT", "/api/v1/books/1", bytes.NewReader(jsonBook))
-
-	router.ServeHTTP(w, req)
-
+	w := execRequest("PUT", "/api/v1/books/1", bytes.NewReader(jsonBook))
 	assert.Equal(t, http.StatusBadRequest, w.Code)
 
-	var rError models.Error
-	err = json.NewDecoder(w.Body).Decode(&rError)
-	assert.NoError(t, err, "Error decoding response data")
+	checkErrorBodyNotEmpty(w, t)
+}
 
-	assert.NotEmpty(t, rError.Error, "Error message should not be empty")
+// PATCH /books
+func TestPatchBookSuccess(t *testing.T) {
+	jsonStr := []byte(`{"title":"Patch book test", "pages":999}`)
+	w := execRequest("PATCH", "/api/v1/books/1", bytes.NewReader(jsonStr))
+	assert.Equal(t, http.StatusNoContent, w.Code)
+}
+
+func TestPatchBookNotFoundBigPathId(t *testing.T) {
+	jsonStr := []byte(`{"title":"Patch book test", "pages":999}`)
+	w := execRequest("PATCH", "/api/v1/books/9999", bytes.NewReader(jsonStr))
+	assert.Equal(t, http.StatusNotFound, w.Code)
+
+	checkErrorBodyNotEmpty(w, t)
+}
+
+func TestPatchBookBadRequestStringPathId(t *testing.T) {
+	jsonStr := []byte(`{"title":"Patch book test", "pages":999}`)
+	w := execRequest("PATCH", "/api/v1/books/string", bytes.NewReader(jsonStr))
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+	checkErrorBodyNotEmpty(w, t)
+}
+
+func TestPatchBookBadRequestBadJSON(t *testing.T) {
+	jsonStr := []byte(`{"title":"Patch book test", "pages":Should be number"}`)
+	w := execRequest("PATCH", "/api/v1/books/1", bytes.NewReader(jsonStr))
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+	checkErrorBodyNotEmpty(w, t)
 }

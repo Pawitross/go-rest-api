9c6404a8dfb535e1066a4e01fcc5ccd986a9e60b
Pawitross <pabialek@us.edu.pl>
2025-06-27 13:56:10 +0200

    Add unit tests for Book struct validation

---
+++ internal/models/book_test.go
@@ -0,0 +1,82 @@
+package models_test
+
+import (
+	"testing"
+
+	"pawrest/internal/models"
+)
+
+func modifyBook(book models.Book, modify func(b *models.Book)) models.Book {
+	b := book
+	modify(&b)
+	return b
+}
+
+func TestBookValidation(t *testing.T) {
+	validBook := models.Book{
+		Id:       1,
+		Title:    "Foo",
+		Year:     1998,
+		Pages:    100,
+		Author:   1,
+		Genre:    1,
+		Language: 1,
+	}
+
+	bookTests := map[string]struct {
+		book      models.Book
+		isInvalid bool
+	}{
+		"Valid": {
+			book:      validBook,
+			isInvalid: false,
+		},
+		"EmptyTitle": {
+			book: modifyBook(validBook, func(b *models.Book) {
+				b.Title = ""
+			}),
+			isInvalid: true,
+		},
+		"ZeroYear": {
+			book: modifyBook(validBook, func(b *models.Book) {
+				b.Year = 0
+			}),
+			isInvalid: true,
+		},
+		"NegativePages": {
+			book: modifyBook(validBook, func(b *models.Book) {
+				b.Pages = -1
+			}),
+			isInvalid: true,
+		},
+		"NegativeAuthor": {
+			book: modifyBook(validBook, func(b *models.Book) {
+				b.Author = -1
+			}),
+			isInvalid: true,
+		},
+		"NegativeGenre": {
+			book: modifyBook(validBook, func(b *models.Book) {
+				b.Genre = -1
+			}),
+			isInvalid: true,
+		},
+		"NegativeLanguage": {
+			book: modifyBook(validBook, func(b *models.Book) {
+				b.Language = -1
+			}),
+			isInvalid: true,
+		},
+	}
+
+	for name, tt := range bookTests {
+		t.Run(name, func(t *testing.T) {
+			actual := tt.book.IsNotValid()
+
+			if tt.isInvalid != actual {
+				t.Errorf("\n    test: %v\nexpected: %v\n     got: %v\n     for: %+v",
+					name, tt.isInvalid, actual, tt.book)
+			}
+		})
+	}
+}

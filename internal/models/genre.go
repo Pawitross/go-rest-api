305b6f5a137290806e43cd9469f15a580165b74d
Pawitross <pabialek@us.edu.pl>
2025-07-08 16:57:26 +0200

    Add validation function to Genre struct and add tests

--- internal/models/genre.go
+++ internal/models/genre.go
@@ -4,3 +4,7 @@ type Genre struct {
 	Id   int64  `json:"id"`
 	Name string `json:"name"`
 }
+
+func (g *Genre) IsNotValid() bool {
+	return g.Name == ""
+}
---
+++ internal/models/genre_test.go
@@ -0,0 +1,40 @@
+package models_test
+
+import (
+	"testing"
+
+	"pawrest/internal/models"
+)
+
+func TestGenreValidation(t *testing.T) {
+	genreTests := map[string]struct {
+		genre     models.Genre
+		isInvalid bool
+	}{
+		"Valid": {
+			genre: models.Genre{
+				Id:   1,
+				Name: "Horror",
+			},
+			isInvalid: false,
+		},
+		"EmptyName": {
+			genre: models.Genre{
+				Id:   1,
+				Name: "",
+			},
+			isInvalid: true,
+		},
+	}
+
+	for name, tt := range genreTests {
+		t.Run(name, func(t *testing.T) {
+			actual := tt.genre.IsNotValid()
+
+			if tt.isInvalid != actual {
+				t.Errorf("\n    test: %v\nexpected: %v\n     got: %v\n     for: %+v",
+					name, tt.isInvalid, actual, tt.genre)
+			}
+		})
+	}
+}

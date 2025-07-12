78b4e50986354eb201766596d51d6feefda1ad16
Pawitross <pabialek@us.edu.pl>
2025-07-12 19:07:43 +0200

    Add validation function to Language struct and add tests

--- internal/models/language.go
+++ internal/models/language.go
@@ -4,3 +4,7 @@ type Language struct {
 	Id   int64  `json:"id"`
 	Name string `json:"name"`
 }
+
+func (l *Language) IsNotValid() bool {
+	return l.Name == ""
+}
---
+++ internal/models/language_test.go
@@ -0,0 +1,40 @@
+package models_test
+
+import (
+	"testing"
+
+	"pawrest/internal/models"
+)
+
+func TestLanguageValidation(t *testing.T) {
+	languageTests := map[string]struct {
+		language  models.Language
+		isInvalid bool
+	}{
+		"Valid": {
+			language: models.Language{
+				Id:   1,
+				Name: "Horror",
+			},
+			isInvalid: false,
+		},
+		"InvalidEmptyName": {
+			language: models.Language{
+				Id:   1,
+				Name: "",
+			},
+			isInvalid: true,
+		},
+	}
+
+	for name, tt := range languageTests {
+		t.Run(name, func(t *testing.T) {
+			actual := tt.language.IsNotValid()
+
+			if tt.isInvalid != actual {
+				t.Errorf("\n    test: %v\nexpected: %v\n     got: %v\n     for: %+v",
+					name, tt.isInvalid, actual, tt.language)
+			}
+		})
+	}
+}
